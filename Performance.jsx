a81218c5cc9da24b2e6ef7bda08a07eb74b0b27f
gunnamlakshmidevi-jpg <gunnamlakshmidevi@gmail.com>
2025-12-26 09:41:23 +0530

    Create Performance.jsx

---
+++ Performance.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Performance() {
+  return (
+    <div>
+      <h1>Performance Analytics</h1>
+      <p>System performance analytics will be shown here.</p>
+    </div>
+  );
+}
